ac3e1d0e93d260c53bb09ab49a4e9f9d9b37d5da
uy <a.normurzaev20@gmail.com>
2026-05-05 23:40:29 +0500

    yangi dizayn

--- src/Caunter.jsx
+++ src/Caunter.jsx
@@ -1,1 +1,20 @@
 
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="panel">
+      <h3>Interaktiv Hisoblagich</h3>
+      <div className="counter-value">{count}</div>
+      <div className="btn-group">
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
