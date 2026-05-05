ae9891d6ec538d6a844afafabc08026bc46809d6
uy <a.normurzaev20@gmail.com>
2026-05-05 23:43:17 +0500

    appjsx bolimi

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import './App.css';
 function App() {
   return (
     <div className="dashboard-container">
-      {/* 1-blok: Hisoblagich */}
+    
       <Counter />
 
-      {/* 2-5 bloklar: Qo'shimcha ma'lumotlar */}
+     
       <InfoCard title="Loyihalar" description="Jami 12 ta tugallangan ishlar." />
       <InfoCard title="Skillset" description="React, Next.js, Tailwind, TypeScript." />
       <InfoCard title="Tajriba" description="1 yillik professional tajriba." />
