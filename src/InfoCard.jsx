7052eb4104b0df9641b8568206abe2ece32b6b79
uy <a.normurzaev20@gmail.com>
2026-05-05 23:41:55 +0500

    infocard bolimi

--- src/InfoCard.jsx
+++ src/InfoCard.jsx
@@ -1,2 +1,14 @@
 
 
+import React from 'react';
+
+const InfoCard = ({ title, description }) => {
+  return (
+    <div className="panel">
+      <h3>{title}</h3>
+      <p style={{ color: '#666', textAlign: 'center' }}>{description}</p>
+    </div>
+  );
+};
+
+export default InfoCard;
